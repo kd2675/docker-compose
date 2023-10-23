54e25be49a84a933a6bf707f6653647651e6cc1b
김도영 <zz01064842675@gmail.com>
2023-10-23 11:28:57 +0900

    test

--- server/init-letsencrypt.sh
+++ server/init-letsencrypt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if ! [ -x "$(command docker compose version)" ]; then
+if ! [ -x "$(docker compose version)" ]; then
   echo 'Error: docker compose is not installed.' >&2
   exit 1
 fi
